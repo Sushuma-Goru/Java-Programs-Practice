6656452ea205a695fbcffeb87ae2129bf279aea2
sushuma <sushumagoru721@gmail.com>
2026-05-02 03:48:57 +0530

    First Java Program

---
+++ InputFromUser.java
@@ -0,0 +1,13 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class InputFromUser {
+    
+    public static void main(String[] args) {
+        
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+         
+    }
+}
