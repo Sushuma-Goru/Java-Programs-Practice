e8c50139ae5e5129e40291c3f6abe97a2db6d2b4
sushuma <sushumagoru721@gmail.com>
2026-05-02 12:30:58 +0530

    added line in main branch

--- Add2Num.java
+++ Add2Num.java
@@ -3,6 +3,7 @@ public class Add2Num {
     public static void main(String[] args) {
         int a=10,b=20;
         System.out.println(a+b);
+        System.out.println("learning to resolve merge conflicts so added this line in main branch");
     }
     
 }
